baa472dd7c9f0046b22d973b4c46618947666971
liyalliya <virtucio.alliyabernadette@ue.edu.ph>
2023-09-20 22:31:47 +0800

    new update

--- fa1_midterm_virtucio/src/Main3.java
+++ fa1_midterm_virtucio/src/Main3.java
@@ -2,7 +2,16 @@ import javax.swing.*;
 
 public class Main3 {
     public static void main(String[] args) {
-        int distance = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter distance travelled (in km): ",
-                "Taxi Fare", JOptionPane.PLAIN_MESSAGE));
+        float distance = Float.parseFloat(JOptionPane.showInputDialog(null,
+                "Enter distance travelled (in km): ", "Taxi Fare", JOptionPane.PLAIN_MESSAGE));
+
+        JOptionPane.showMessageDialog(null, "Your total fare: \n" + calculateFare(distance));
+
     }
+    public static float calculateFare(float distance) {
+        float FARE = 60;
+        float totalFare = FARE + (distance * 25.5f);
+        return totalFare;
+    }
+
 }
